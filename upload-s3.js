fe5693ae458b871f0bfb1293d5b94b6827820fd6
ismaelash <contato@ismaelnascimento.com>
2021-12-16 20:48:39 -0300

    [ADDED] get args

--- upload-s3.js
+++ upload-s3.js
@@ -1,1 +1,8 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
 console.log('works fine')
+
+console.log(argv.accessKey)
+console.log(argv.secretKey)
