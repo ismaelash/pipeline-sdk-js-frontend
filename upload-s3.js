4a72d334ae400866f4810d1044f19cb492f9ddd5
ismaelash <contato@ismaelnascimento.com>
2021-12-16 21:09:12 -0300

    [ADDED] upload to s3

--- upload-s3.js
+++ upload-s3.js
@@ -1,8 +1,42 @@
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
 
-console.log('works fine')
+const fs = require("fs");
+const AWS = require("aws-sdk");
+const JavaScriptObfuscator = require("javascript-obfuscator");
+AWS.config.update({
+  region: "us-east-1",
+  accessKeyId: argv.accessKey, // via terminal
+  secretAccessKey: argv.secretKey, // via terminal
+});
+const S3 = new AWS.S3();
 
-console.log(`_${argv.accessKey}_`)
-console.log(argv.secretKey)
+function obfuscatorIO(code) {
+  var obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
+    optionsPreset: "medium-obfuscation",
+    log: true,
+  });
+  return obfuscationResult.getObfuscatedCode();
+}
+
+function uploadFileToS3(code, bucketName, fileNameWithExtesion) {
+  const params = {
+    ACL: "public-read",
+    Body: obfuscatorIO(code),
+    ContentType: "text/javascript",
+    Bucket: bucketName,
+    Key: `${fileNameWithExtesion}`,
+  };
+  // console.log("uploadFileToS3.params", params);
+
+  S3.putObject(params, (err, data) => {
+    if (err) {
+      throw new Error(err);
+    } else {
+      console.log(`uploadFileToS3(${params.Key}) on bucket(${params.Bucket})`);
+    }
+  });
+}
+
+uploadFileToS3('Ismael', 'pipeline-sdk', 'github-actions.js')
